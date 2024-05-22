04f19867feb653a1f49b66a726750a80f2d17d06
TJ-programmer <98687549+TJ-programmer@users.noreply.github.com>
2024-05-22 11:02:41 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,8 @@
 #write a program to find the maximum of two numbers
+n1,n2=map(int,input("Enter two number:").split())
+if n1>n2:
+    print("maximum is",n1)
+elif n1==n2:
+    print("both are equal")
+else:
+    print("maximum is",n2)
